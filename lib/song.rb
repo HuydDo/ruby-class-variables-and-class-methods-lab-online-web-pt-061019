e2734ef29f80747f2574e9602509e5ce3278b8b1
Huy Do <huyddo40@gmail.com>
2019-06-14 17:05:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   def self.artist_count
     # hash = {}
     # @@artist.each do |name|
-    #   if hash(name) 
+    #   if hash(name)
     #     hash(name) +=1
     #   else
     #     hash(name) = 1
